8e6b0b637fff2800fba01c57c075193e6da1e18a
Richard Davis <rdavis.bacs@gmail.com>
2013-08-28 13:30:02 -0400

    Phase II complete

--- kickball.rb
+++ kickball.rb
@@ -1,19 +1,27 @@
-scores = {}
-(1..2).each do |i|
-  loop do
-    print "What was team #{i}'s name?\n> "
-    team_name = gets.chomp
-    print "What was team #{i}'s score?\n> "
-    score = gets.chomp.to_i
+def get_scores
+  scores = {}
+  (1..2).each do |i|
+    loop do
+      print "What was team #{i}'s name?\n> "
+      team_name = gets.chomp
+      print "What was team #{i}'s score?\n> "
+      score = gets.chomp.to_i
 
-    if !scores.keys.include?(team_name)
-      scores[team_name] = score
-      break
-    else
-      puts "The team name '#{team_name}'' has already been used!"
+      if !scores.keys.include?(team_name)
+        scores[team_name] = score
+        break
+      else
+        puts "The team name '#{team_name}'' has already been used!"
+      end
     end
   end
+  scores
 end
 
-victor = scores.sort_by { |name, score| score }.last[0]
+def identify_winner(scores)
+  scores.sort_by { |name, score| score }.last[0]
+end
+
+
+victor = identify_winner(get_scores)
 puts "#{victor} is the Victor!"
