c02c58ee26fbe8af7a165aa6664a35b315e63f23
Richard Davis <rdavis.bacs@gmail.com>
2013-08-28 14:56:40 -0400

    Started an Object Oriented kickball.rb

--- kickball.rb
+++ kickball.rb
@@ -20,21 +20,40 @@ def get_scores
 end
 
 def identify_winner(scores)
-  scores.sort_by { |name, score| score }.last[0]
+  scores.sort_by { |team, score| score }.last[0]
 end
 
-
+# loop for entering games
 games = []
+puts ""
 loop do
   games << get_scores
-  #victor = identify_winner(games.last)
-  #puts "#{victor} is the Victor!\n\n"
-
   print "\nWould you like to provide another game? (Y/N)\n> "
   response = gets.chomp.upcase
   break if response == 'N'
 end
 
+# print out winners, create win/loss record
+win_loss_record = {}
 games.each_with_index do |game, i|
-  puts "\nIn game #{i+1}, #{identify_winner(game)} is the victor!"
+  victor = identify_winner(game)
+  puts "In game #{i+1}, #{victor} is the victor!"
+
+  game.each do |team, score|
+    win_loss_record[team] = {w:0, l:0} unless win_loss_record.keys.include?(team)
+
+    if team == victor
+      win_loss_record[team][:w] += 1
+    else
+      win_loss_record[team][:l] += 1
+    end
+  end
 end
+
+p win_loss_record
+#sorted_records = win_loss_record.sort_by { |team, win_loss| win_loss[:w] }.to_h
+#p sorted_records
+
+# games.each do |game|
+
+# end
---
+++ oo_kickball.rb
@@ -0,0 +1,14 @@
+class Game
+  def initialize(team1, score1, team2, score2)
+    @team1 = team1
+    @team2 = team2
+    @score1 = score1
+    @score2 = score2
+  end
+
+
+end
+
+class Team
+
+end
